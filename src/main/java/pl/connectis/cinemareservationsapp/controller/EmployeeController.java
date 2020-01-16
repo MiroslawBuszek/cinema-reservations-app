59863b6e9997b154c2805ad1da53d3881b36310e
Grzegorz Witkowski <grisza.karamazow@gmail.com>
2020-01-16 11:40:28 +0100

    Adds service for Employee class

--- src/main/java/pl/connectis/cinemareservationsapp/controller/EmployeeController.java
+++ src/main/java/pl/connectis/cinemareservationsapp/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.connectis.cinemareservationsapp.model.Employee;
 import pl.connectis.cinemareservationsapp.repository.EmployeeRepository;
+import pl.connectis.cinemareservationsapp.service.EmployeeService;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -12,21 +13,21 @@ import java.util.List;
 public class EmployeeController {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
 
     @GetMapping("/employee/all")
     public Iterable<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
+        return employeeService.findAll();
     }
 
     @GetMapping("/employee/{id}")
     public List<Employee> getEmployeeById(@PathVariable long id) {
-        return employeeRepository.findById(id);
+        return employeeService.findById(id);
     }
 
     @PostMapping("/employee")
     public Employee addEmployee(@Valid @RequestBody Employee employee) {
-        return employeeRepository.save(employee);
+        return employeeService.save(employee);
     }
 
 }
---
+++ src/main/java/pl/connectis/cinemareservationsapp/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package pl.connectis.cinemareservationsapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import pl.connectis.cinemareservationsapp.model.Employee;
+import pl.connectis.cinemareservationsapp.repository.EmployeeRepository;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public Iterable<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    public List<Employee> findById(long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+}
