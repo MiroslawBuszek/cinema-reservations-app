6961aa01a1fbdfaaaed366aeb19e9ba449d35074
Grzegorz Witkowski <grisza.karamazow@gmail.com>
2020-01-18 23:45:34 +0100

    Adds data.sql with client, employee and movie tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,9 @@ insert into employee (first_name, last_name, login) values
 ('Franciszek', 'Walentowicz', 'fwalentynowicz'),
 ('Piotr', 'Krakowski', 'pkrakowski');
 
+insert into movie (title, category, length, description, age_limit ) values
+('Gwiezdne wojny: Skywalker. Odrodzenie', 'Familijny', 142, 'Członkowie organizacji Ruchu Oporu ponownie stawiają czoła Najwyższemu Porządkowi.', 10),
+('Jak zostałem gangsterem. Historia prawdziwa', 'Akcja', 140, 'Historia najniebezpieczniejszego gangstera w Polsce, dla którego władza, bycie ponad stan i pieniądze stanowią priorytet.', 15),
+('Judy', 'Dramat', 118, 'Zima 1968 roku. Ciesząca się ogromną popularnością Judy Garland przybywa do Londynu na serię koncertów.', 15),
+('Oficer i szpieg', 'Thriller szpiegowski', 132, 'Historia francuskiego kapitana Alfreda Dreyfusa, który został niesłusznie oskarżony o zdradę stanu i skazany na dożywotnie więzienie.', 15),
+('Jumanji: Przygoda w dżungli', 'Familijny', 123, 'Wielki powrót bohaterów „Jumanji: Przygoda w dżungli”! Pierwsza część filmu stała się międzynarodowym przebojem, zarabiając na całym świecie ponad 960 milionów dolarów.', 12);
