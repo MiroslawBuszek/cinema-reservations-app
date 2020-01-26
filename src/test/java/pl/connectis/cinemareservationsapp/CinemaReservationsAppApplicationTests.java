a60084ec3873afa6d2c908b4b1117f66b2f3115e
Mirosław Buszek <zoowett@gmail.com>
2020-01-26 12:25:44 +0100

    Add get client endpoint test

--- src/test/java/pl/connectis/cinemareservationsapp/CinemaReservationsAppApplicationTests.java
+++ src/test/java/pl/connectis/cinemareservationsapp/CinemaReservationsAppApplicationTests.java
@@ -1,13 +1,40 @@
 package pl.connectis.cinemareservationsapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CinemaReservationsAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void ifGetClientRequestIsCorrectShouldReturnCode200() throws Exception {
+        mockMvc.perform(get("/client/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(matchAll(
+                        status().is2xxSuccessful(),
+                        content().json("[\n" +
+                                "    {\n" +
+                                "        \"id\": 1,\n" +
+                                "        \"firstName\": \"Adrian\",\n" +
+                                "        \"lastName\": \"Budny\",\n" +
+                                "        \"age\": 39,\n" +
+                                "        \"email\": \"adrian.budny@gmail.com\"\n" +
+                                "    }\n" +
+                                "]")
+                ));
+    }
 }
