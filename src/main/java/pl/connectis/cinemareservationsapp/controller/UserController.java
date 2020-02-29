7413d4e3c0b51ad3d4da0c9a2584cfeb3cccdd16
Mirosław Buszek <zoowett@gmail.com>
2020-02-29 22:13:21 +0100

    Add user & movie test cases

--- src/main/java/pl/connectis/cinemareservationsapp/controller/UserController.java
+++ src/main/java/pl/connectis/cinemareservationsapp/controller/UserController.java
@@ -1,17 +1,28 @@
 package pl.connectis.cinemareservationsapp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import pl.connectis.cinemareservationsapp.exceptions.BadRequestException;
 import pl.connectis.cinemareservationsapp.exceptions.ResourceNotFoundException;
 import pl.connectis.cinemareservationsapp.model.User;
 import pl.connectis.cinemareservationsapp.security.IAuthenticationFacade;
 import pl.connectis.cinemareservationsapp.service.UserService;
 
-import javax.validation.Valid;
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -22,7 +33,6 @@ public class UserController {
     @Autowired
     private IAuthenticationFacade authenticationFacade;
 
-
     @RequestMapping(value = "/username", method = RequestMethod.GET)
     @ResponseBody
     public String currentUserNameSimple() {
@@ -32,22 +42,21 @@ public class UserController {
 
     @PostMapping("/registration/client")
     public ResponseEntity<User> addUserClient(@Valid @RequestBody User user) {
-        if (userService.findByUsername(user.getUsername()) != null) {                   //todo
+        if (userService.findByUsername(user.getUsername()) != null) { // todo
             throw new BadRequestException("user {username=" + user.getUsername() + "} was found");
         }
-        if (user.getRoles().equals("CLIENT") == false){
+        if (user.getRoles().equals("CLIENT") == false) {
             throw new BadRequestException("{Incorrect role: this is not a customer}");
         }
         return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
     }
 
-
     @PostMapping("/registration/employee")
     public ResponseEntity<User> addUserEmployee(@Valid @RequestBody User user) {
-        if (userService.findByUsername(user.getUsername()) != null) {                   //todo
+        if (userService.findByUsername(user.getUsername()) != null) { // todo
             throw new BadRequestException("user {username=" + user.getUsername() + "} was found");
         }
-        if (user.getRoles().equals("EMPLOYEE") == false){
+        if (user.getRoles().equals("EMPLOYEE") == false) {
             throw new BadRequestException("{Incorrect role: this is not an employee}");
         }
         return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
@@ -67,9 +76,8 @@ public class UserController {
         return user;
     }
 
-
     @PostMapping("/many")
-    public ResponseEntity<Iterable> addUserList(@Valid @RequestBody Iterable<User> userList) {
+    public ResponseEntity<?> addUserList(@Valid @RequestBody Iterable<User> userList) {
         for (User user : userList) {
             if (userService.findByUsername(user.getUsername()) != null) {
                 throw new BadRequestException("user {username=" + user.getUsername() + "} was found");
@@ -84,21 +92,21 @@ public class UserController {
         if (existingUser == null) {
             throw new ResourceNotFoundException("user {id=" + id + "} was not found");
         } else if (!user.getUsername().equals(existingUser.getUsername())) {
-            throw new BadRequestException("{username=" + user.getUsername() +
-                    "} does not correspond to user of {id=" + id + "}");
+            throw new BadRequestException(
+                    "{username=" + user.getUsername() + "} does not correspond to user of {id=" + id + "}");
         } else {
             return new ResponseEntity<>(userService.updateById(id, user), HttpStatus.CREATED);
         }
     }
 
     @DeleteMapping
-    public ResponseEntity deleteUser(@RequestParam long id) {
+    public ResponseEntity<?> deleteUser(@RequestParam long id) {
         User user = userService.findById(id);
         if (user == null) {
             throw new ResourceNotFoundException("user {id=" + id + "} was not found");
         }
         userService.deleteById(id);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
