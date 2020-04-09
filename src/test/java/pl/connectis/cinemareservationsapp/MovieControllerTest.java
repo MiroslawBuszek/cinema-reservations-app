15501c887e3f557ea13bd611f3fe7eedd887a687
Grzegorz Witkowski <grisza.karamazow@gmail.com>
2020-04-09 16:49:06 +0200

    Fixes and refactors MovieControllerTest

--- src/test/java/pl/connectis/cinemareservationsapp/MovieControllerTest.java
+++ src/test/java/pl/connectis/cinemareservationsapp/MovieControllerTest.java
@@ -1,5 +1,11 @@
 package pl.connectis.cinemareservationsapp;
 
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,68 +16,97 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.Properties;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class MovieControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/allMovie.csv", delimiter = ';')
-    public void findAllMovie(String content) throws Exception {
-        mockMvc.perform(get("/movie/all").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(matchAll(status().is2xxSuccessful(), content().json(content)));
+    @BeforeAll
+    public static void setSpringProfile() {
+        Properties properties = System.getProperties();
+        properties.setProperty("spring.profiles.active", "develop");
     }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/findMovieById.csv", delimiter = ';')
-    public void findMovieById(long id, String content) throws Exception {
-        mockMvc.perform(get("/movie/?id={id}", id).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(matchAll(status().is2xxSuccessful(), content().json(content)));
+    @AfterAll
+    public static void resetSpringProfile() {
+        System.clearProperty("spring.profiles.active");
     }
 
+    @Order(1)
     @ParameterizedTest
-    @CsvFileSource(resources = "/addNewMovie.csv", delimiter = ';')
-    public void addNewMovie(String content) throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/movie").content(content)
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/addListOfNewMovie.csv", delimiter = ';')
-    public void addListOfNewMovie(String content) throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/movie/many").content(content)
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/updateMovie.csv", delimiter = ';')
-    public void updateMovie(long id, String content) throws Exception {
+    @CsvFileSource(resources = "/movie/getMovieByExampleAll.csv", delimiter = ';')
+    public void getMovieByExampleAll(String response) throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
-                .put("/movie/?id={id}", id)
-                .content(content)
+                .get("/movie")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(response));
+    }
+
+    @Order(2)
+    @ParameterizedTest
+    @CsvFileSource(resources = "/movie/getMovieByExampleCategory.csv", delimiter = ';')
+    public void getMovieByExampleCategory(String category, String response) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/movie?category={category}", category)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(response));
+    }
+
+    @Order(3)
+    @ParameterizedTest
+    @CsvFileSource(resources = "/movie/getMovieByExampleTitle.csv", delimiter = ';')
+    public void getMovieByExampleTitle(String title, String response) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/movie?title={title}", title)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(response));
+    }
+
+    @Order(4)
+    @ParameterizedTest
+    @CsvFileSource(resources = "/movie/addMovie.csv", delimiter = ';')
+    public void addMovie(String request, String response) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/movie")
+                .content(request)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("3 idiots"));
+                .andExpect(content().json(response))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
     }
 
+    @Order(5)
     @ParameterizedTest
-    @CsvFileSource(resources = "/deleteMovieById.csv", delimiter = ';')
+    @CsvFileSource(resources = "/movie/updateMovie.csv", delimiter = ';')
+    public void updateMovie(String request, String response) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .put("/movie")
+                .content(request)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(response));
+    }
+
+    @Order(6)
+    @ParameterizedTest
+    @CsvFileSource(resources = "/movie/deleteMovie.csv", delimiter = ';')
     public void deleteMovie(long id) throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
-                .delete("/movie/?id={id}", id)
+                .delete("/movie?id={id}", id)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
