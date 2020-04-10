f5275e05224234a1aaa9a70b6e58bad63562f17b
Grzegorz Witkowski <grisza.karamazow@gmail.com>
2020-04-10 19:07:13 +0200

    Cleans up the code

--- src/test/java/pl/connectis/cinemareservationsapp/RoomControllerTest.java
+++ src/test/java/pl/connectis/cinemareservationsapp/RoomControllerTest.java
@@ -53,6 +53,17 @@ public class RoomControllerTest {
 
     @Order(2)
     @ParameterizedTest
+    @CsvFileSource(resources = "/room/getRoomByCapacity.csv", delimiter = ';')
+    public void getRoomByCapacity(int capacity, String response) throws Exception {
+        mockMvc.perform(get("/room?capacity={capacity}", capacity)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(response))
+                .andDo(print());
+    }
+
+    @Order(3)
+    @ParameterizedTest
     @CsvFileSource(resources = "/room/addRoom.csv", delimiter = ';')
     public void addRoom(String request, String response) throws Exception {
         mockMvc.perform(post("/room")
@@ -64,7 +75,31 @@ public class RoomControllerTest {
                 .andDo(print());
     }
 
-    @Order(3)
+    @Order(4)
+    @ParameterizedTest
+    @CsvFileSource(resources = "/room/addRoomWrongCapacity.csv", delimiter = ';')
+    public void addRoomWrongCapacity(String request) throws Exception {
+        mockMvc.perform(post("/room")
+                .content(request)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Order(5)
+    @ParameterizedTest
+    @CsvFileSource(resources = "/room/addRoomInappropriateLayoutFormat.csv", delimiter = ';')
+    public void addRoomInappropriateLayoutFormat(String request) throws Exception {
+        mockMvc.perform(post("/room")
+                .content(request)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Order(6)
     @ParameterizedTest
     @CsvFileSource(resources = "/room/updateRoom.csv", delimiter = ';')
     public void updateRoom(String request, String response) throws Exception {
@@ -77,7 +112,7 @@ public class RoomControllerTest {
                 .andDo(print());
     }
 
-    @Order(4)
+    @Order(7)
     @ParameterizedTest
     @CsvFileSource(resources = "/room/deleteRoom.csv", delimiter = ';')
     public void deleteRoom(long id) throws Exception {
@@ -85,6 +120,29 @@ public class RoomControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @Order(8)
+    @ParameterizedTest
+    @CsvFileSource(resources = "/room/deleteRoomDoesntExists.csv", delimiter = ';')
+    public void deleteRoomDoesntExists(long id) throws Exception {
+        mockMvc.perform(delete("/room?id={id}", id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Order(9)
+    @ParameterizedTest
+    @CsvFileSource(resources = "/room/updateRoomDoesntExists.csv", delimiter = ';')
+    public void updateRoomDoesntExists(String request) throws Exception {
+        mockMvc.perform(put("/room")
+                .content(request)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
                 .andDo(print());
     }
 
